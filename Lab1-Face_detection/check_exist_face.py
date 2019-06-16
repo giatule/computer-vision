a6ef64ddf3f28a5cc7eb03aef0bd03273a5bf5fc
Andy lee <giatule@gmail.com>
2019-06-17 00:59:06 +0700

    Update check_exist_face.py

--- Lab1-Face_detection/check_exist_face.py
+++ Lab1-Face_detection/check_exist_face.py
@@ -1,15 +1,16 @@
 import face_recognition
-# Load in our reference image of Joe Biden
-known_image = face_recognition.load_image_file("train/Le Gia Tu/21. Le Gia Tu.jpg")
-# Load in our image of a group of people
-unknown_image = face_recognition.load_image_file("stock_people.jpg")
+# Load in our hinh anh tham chieu cua Le gia Tu
+known_image = face_recognition.load_image_file("Lab1-Face_detection/train/Le Gia Tu/21. Le Gia Tu.jpg")
+# Load Load một hình nhóm người bất kỳ
+unknown_image = face_recognition.load_image_file("Lab1-Face_detection/Input/lab2.jpg")
 
-# Create a biden encoding
-biden_encoding = face_recognition.face_encodings(known_image)[0]
-# create an encoding based off our group photo
+# tạo một bộ encoding tu hinh mẫu
+Tu_encoding = face_recognition.face_encodings(known_image)[0]
+# tạo encoding cho hình nhóm người bất kỳ (hình chưa xác định)
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-# Compare the encodings and try to determine if Biden exists within a photo
-results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+# So sánh đối chiếu encoding mấu với bộ encoding chưa xác định nếu khớp --> true/fo
+results = face_recognition.compare_faces([Tu_encoding], unknown_encoding)
 # Print the results
+print("Kết quả nhận diện ")
 print(results)
